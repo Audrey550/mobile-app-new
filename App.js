147611eb4b1c0aaba33edc9bfbf0b062214fd2c6
Audrey <r0982547@student.thomasmore.be>
2025-06-14 19:25:05 +0200

    Feat: added products to the bottom navigation tab

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -15,36 +14,45 @@ import ProductScreen from './screens/ProductScreen.js';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const BottomTabNavigator = () => {
+  return (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+        if (route.name === 'Home') {
+          iconName = 'home';
+        } else if (route.name === 'Products') {
+          iconName = 'list';
+        } else if (route.name === 'Cart') {
+          iconName = 'cart';
+        }
+        return <Icon name={iconName} size={size} color={color} />;
+      }
+    })}
+  >
+  <Tab.Screen name="Home" component={HomeScreen} />
+  <Tab.Screen name="Products" component={ProductScreen} />
+  <Tab.Screen name="Cart" component={CartScreen} />
+  </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <CartProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeTabs" options={{headerShown: false}}>
-        {() => (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({color, size}) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Cart') {
-              iconName = 'cart';
-            }
-            return <Icon name={iconName} size={size} color={color} />;
-          }
-        })}
-        >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Cart" component={CartScreen} />
-      </Tab.Navigator>
-      )}
-        </Stack.Screen>
-        <Stack.Screen name="Products" component={ProductScreen} />
-        <Stack.Screen name="Details" component={ProductDetail} />
-        <Stack.Screen name="Blog" component={BlogDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeTabs" options={{headerShown: false}}>
+          {() => (
+            <BottomTabNavigator />
+          )}
+          </Stack.Screen>
+          <Stack.Screen name="Products" component={ProductScreen} />
+          <Stack.Screen name="Details" component={ProductDetail} />
+          <Stack.Screen name="Blog" component={BlogDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </CartProvider>
   );
 }
